b43278657e0118d4d6357e8e493fbe2f35dcbe66
Elodie De Cola <ede-cola@bess-f1r5s13.clusters.42paris.fr>
2023-11-03 13:42:29 +0100

    ft_memcmp

---
+++ ft_memcmp.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ede-cola <ede-cola@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/03 13:30:06 by ede-cola          #+#    #+#             */
+/*   Updated: 2023/11/03 13:41:20 by ede-cola         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n && (((unsigned char *)s1)[i] || ((unsigned char *)s2)[i]))
+	{
+		if (((unsigned char *)s1)[i] != ((unsigned char *)s2)[i])
+			return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
+		i++;
+	}
+	return (0);
+}
