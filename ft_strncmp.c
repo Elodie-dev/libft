90528775e4209c6d07fb6e7c2a1b88e6557772ab
Elodie De Cola <ede-cola@bess-f1r5s13.clusters.42paris.fr>
2023-11-03 13:10:42 +0100

    ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,13 +6,23 @@
 /*   By: ede-cola <ede-cola@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 12:11:38 by ede-cola          #+#    #+#             */
-/*   Updated: 2023/11/03 12:12:48 by ede-cola         ###   ########.fr       */
+/*   Updated: 2023/11/03 13:10:21 by ede-cola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include <string.h>
 
-int ft_strncmp(const char *s1, const char *s2, size_t n)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-    
+	size_t	i;
+
+	i = 0;
+	while (i < n && (s1[i] || s2[i]))
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
 }
