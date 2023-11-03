8a79c08d71a440cdc8177da2925d88f3c54685fe
Elodie De Cola <ede-cola@bess-f1r5s13.clusters.42paris.fr>
2023-11-03 16:05:08 +0100

    ft_strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -1,25 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ede-cola <ede-cola@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/03 15:35:03 by ede-cola          #+#    #+#             */
-/*   Updated: 2023/11/03 16:04:48 by ede-cola         ###   ########.fr       */
+/*   Created: 2023/11/03 15:47:09 by ede-cola          #+#    #+#             */
+/*   Updated: 2023/11/03 16:04:35 by ede-cola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-void	*ft_calloc(size_t nmemb, size_t size)
+char	*ft_strdup(const char *s)
 {
-	void	*ret;
+	int		i;
+	char	*ret;
 
-	ret = malloc(nmemb * size);
+	i = 0;
+	while (s[i])
+		i++;
+	ret = malloc((i * sizeof(char)) + 1);
 	if (ret == NULL)
-		retrun(0);
+		return (0);
+	i = 0;
+	while (s[i])
+	{
+		ret[i] = s[i];
+		i++;
+	}
+	ret[i] = '\0';
 	return (ret);
 }
