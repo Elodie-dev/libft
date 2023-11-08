2e4942440f44f53a7e1ad8d9eb9be3110fbd8645
Elodie De Cola <ede-cola@paul-f3Br5s4.clusters.42paris.fr>
2023-11-08 12:49:05 +0100

    calloc corrected

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: ede-cola <ede-cola@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 15:35:03 by ede-cola          #+#    #+#             */
-/*   Updated: 2023/11/07 19:57:53 by ede-cola         ###   ########.fr       */
+/*   Updated: 2023/11/08 12:48:36 by ede-cola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,15 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*ret;
 
+	if (nmemb * size == 0)
+	{
+		ret = malloc(0);
+		if (!ret)
+			return (NULL);
+		return (ret);
+	}
+	else if (!nmemb || !size || (nmemb * size) / size != nmemb)
+		return (NULL);
 	ret = malloc(nmemb * size);
 	if (!ret)
 		return (NULL);
