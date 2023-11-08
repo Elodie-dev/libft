21c694239ebb398989b319888efb465283bd9da2
Elodie De Cola <ede-cola@paul-f3Br5s4.clusters.42paris.fr>
2023-11-08 09:44:30 +0100

    strlcpy corrected

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: ede-cola <ede-cola@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 17:13:18 by ede-cola          #+#    #+#             */
-/*   Updated: 2023/11/07 19:04:41 by ede-cola         ###   ########.fr       */
+/*   Updated: 2023/11/08 09:43:29 by ede-cola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,15 @@
 size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
 	size_t	i;
-	size_t	j;
 
 	i = 0;
-	j = 0;
-	if (size > 0)
-	{
-		while (src[j])
-			j++;
-	}
+	if (size == 0)
+		return (ft_strlen(src));
 	while (i < size - 1 && src[i])
 	{
 		dest[i] = src[i];
 		i++;
 	}
 	dest[i] = '\0';
-	return (j);
+	return (ft_strlen(src));
 }
