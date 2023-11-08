6612784f74953e0e401c8a6794b373a60332708e
Elodie De Cola <ede-cola@paul-f3Br5s4.clusters.42paris.fr>
2023-11-08 15:13:12 +0100

    1er partie ok

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: ede-cola <ede-cola@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 17:56:54 by ede-cola          #+#    #+#             */
-/*   Updated: 2023/11/08 10:37:22 by ede-cola         ###   ########.fr       */
+/*   Updated: 2023/11/08 15:10:42 by ede-cola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,26 +18,16 @@ size_t	ft_strlcat(char *dest, const char *src, size_t size)
 	size_t	j;
 
 	i = 0;
+	while (dest[i] && i < size)
+		i++;
+	if (i >= size)
+		return (i + ft_strlen(src));
 	j = 0;
-	if (size == 0)
-		return (ft_strlen(dest));
-	else
+	while (j < (size - i - 1) && src[j])
 	{
-		while (i < size - 1)
-			i++;
-		if (dest[i] != '\0')
-			return (ft_strlen(dest) + size);
-		else
-		{
-			if (size > i + 1)
-			{
-				while (j < size - i)
-					dest[i++] = src[j++];
-				dest[i] = '\0';
-				return (ft_strlen(dest) + j);
-			}
-			else
-				return (ft_strlen(dest));
-		}
+		dest[i + j] = src[j];
+		j++;
 	}
+	dest[i + j] = '\0';
+	return (i + ft_strlen(src));
 }
