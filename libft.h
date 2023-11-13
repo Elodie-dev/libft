4da8be921bde0ad48fa901ea17f5eb1ed05ef1ba
Elodie De Cola <ede-cola@paul-f3Br6s4.clusters.42paris.fr>
2023-11-13 14:37:10 +0100

    ft_striteri ok

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ede-cola <ede-cola@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 15:33:02 by ede-cola          #+#    #+#             */
-/*   Updated: 2023/11/13 14:20:21 by ede-cola         ###   ########.fr       */
+/*   Updated: 2023/11/13 14:27:42 by ede-cola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,5 +48,6 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 
 #endif
