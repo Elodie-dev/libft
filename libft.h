aa6e01dc8f2849149cb67fb6d27e6fa19a6f859d
Elodie De Cola <ede-cola@made-f0Ar1s8.clusters.42paris.fr>
2023-11-14 12:30:37 +0100

    bonus lstlast

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ede-cola <ede-cola@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 15:33:02 by ede-cola          #+#    #+#             */
-/*   Updated: 2023/11/14 12:01:11 by ede-cola         ###   ########.fr       */
+/*   Updated: 2023/11/14 12:15:22 by ede-cola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,5 +64,6 @@ typedef struct s_list
 t_list				*ft_lstnew(void *content);
 void				ft_lstadd_front(t_list **lst, t_list *new);
 int					ft_lstsize(t_list *lst);
+t_list				*ft_lstlast(t_list *lst);
 
 #endif
