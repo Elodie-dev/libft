05d52316cd97534eab2c07ef77e06cbc692111bc
Elodie De Cola <ede-cola@made-f0Ar1s8.clusters.42paris.fr>
2023-11-14 15:00:55 +0100

    bonus lstiter ok

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ede-cola <ede-cola@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 15:33:02 by ede-cola          #+#    #+#             */
-/*   Updated: 2023/11/14 13:51:49 by ede-cola         ###   ########.fr       */
+/*   Updated: 2023/11/14 14:54:02 by ede-cola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,5 +68,6 @@ t_list				*ft_lstlast(t_list *lst);
 void				ft_lstadd_back(t_list **lst, t_list *new);
 void				ft_lstdelone(t_list *lst, void (*del)(void *));
 void				ft_lstclear(t_list **lst, void (*del)(void *));
+void				ft_lstiter(t_list *lst, void (*f)(void *));
 
 #endif
